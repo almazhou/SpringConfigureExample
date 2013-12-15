cb5feed3815af8cd75e8ca173b583a6a94dff898
almazhou <zx_new@126.com>
2013-12-15 14:46:38 +0800

    add XmlViewResolver

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -4,13 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
+
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
 		return "hello";
 	}
+
+    @RequestMapping(value = "/testView",method = RequestMethod.GET)
+	public ModelAndView testMyView() {
+		return new ModelAndView("myView");
+	}
+
+    @RequestMapping(value = "/testRedirect",method = RequestMethod.GET)
+    public ModelAndView welcomeRedirect() {
+        return new ModelAndView("welcomeRedirect");
+    }
 }
---
+++ src/main/webapp/WEB-INF/pages/customise.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: twer
+  Date: 12/15/13
+  Time: 2:43 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+      <h1>This is a view using XmlVeiwResolver</h1>>
+</body>
+</html>
